6de09ebbd3d033170a12526cdce3dffee1d2b4c2
srikar <srikar071@gmail.com>
2026-06-20 10:54:29 +0530

    sending email feture bugs 6.3 fixed

--- backend/controllers/JobRequestController.js
+++ backend/controllers/JobRequestController.js
@@ -146,7 +146,7 @@ exports.sendCandidateEmail = async (req, res) => {
     console.log("ENV USER:", process.env.EMAIL_USER);
     console.log("ENV PASS:", process.env.EMAIL_PASS);
 
-    const candidateLink = `https://nectarshall-api-fhcpggc7gxcnbbhq.southindia-01.azurewebsites.net/api/candidate-form/${request.caseId}`;
+    const candidateLink = `https://purple-sand-0241d5e00.7.azurestaticapps.net/candidate-form/${request.caseId}`;
     console.log("EMAIL:", request.email);
     console.log("REQUEST:", request);
     await transporter.sendMail({
