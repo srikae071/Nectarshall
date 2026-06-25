8025d4570594fa6b9da837c45dca314d6fbad4d3
srikar <srikar071@gmail.com>
2026-06-25 21:12:08 +0530

    offBordingfix 2.3.2

--- Client/src/pages/Hrms/OnBoarding/OnBoardingClinet/CandidateForm/CandiateFormNav/index.jsx
+++ Client/src/pages/Hrms/OnBoarding/OnBoardingClinet/CandidateForm/CandiateFormNav/index.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import logo from "../../../../images/logo.png";
+import logo from "../../../../../../images/logo.png";
 import "./index.css";
 
 function CandiateFormNav() {
