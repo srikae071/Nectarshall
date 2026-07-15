78da290ab62667da4aca6badba22b669b7744367
srikar <srikar071@gmail.com>
2026-07-15 20:07:42 +0530

    test 1.2.4.3

--- backend/routes/BoardingRoutes.js
+++ backend/routes/BoardingRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../middleware/multer");
 
 const {
   createBoarding,
@@ -9,12 +8,12 @@ const {
   updateBoarding,
 } = require("../controllers/BoardingController");
 
-router.post("/create", upload.single("attachment"), createBoarding);
+router.post("/create", createBoarding);
 
 router.get("/", getAllBoardings);
 
 router.get("/:id", getBoardingById);
 
-router.put("/:id", upload.single("attachment"), updateBoarding);
+router.put("/:id", updateBoarding);
 
 module.exports = router;
